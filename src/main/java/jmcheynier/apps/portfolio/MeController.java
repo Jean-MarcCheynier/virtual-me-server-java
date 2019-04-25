a25f134d0d0045c54be450d5d0d009c4eb12fadf
Jean-Marc <jmcheynier@gmail.com>
2019-04-25 11:57:12 +0200

    fix query loading

--- src/main/java/jmcheynier/apps/portfolio/MeController.java
+++ src/main/java/jmcheynier/apps/portfolio/MeController.java
@@ -1,28 +1,49 @@
 package jmcheynier.apps.portfolio;
 
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jmcheynier.apps.portfolio.services.GithubService;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @RestController
 @RequestMapping("/me")
 public class MeController {
+	
+	@Autowired
+	GithubService githubService;
 
-    @RequestMapping("/")
-    public Me index() {
-    	Me me = new Me();
-    	me.setName("undefined");
-        return me;
-    }
-    
-    @RequestMapping("/name/{name}")
-    public Me index(@PathVariable String name) {
-    	Me me = new Me();
-    	me.setName(name);
-        return me;
-    }
+	public static final MediaType JSON  = MediaType.get("application/json; charset=utf-8");
+
+	@RequestMapping("/")
+	public Me index() {
+		Me me = new Me();
+		me.setName("undefined");
+		return me;
+	}
+
+	@RequestMapping("/name/{name}")
+	public Me index(@PathVariable String name) {
+		Me me = new Me();
+		me.setName(name);
+		return me;
+	}
+
+	@RequestMapping("/comments")
+	public String commentsGithubGraphQL() throws IOException {
+		return githubService.getGithubTags();
+
+	}
+
+
 
 }
---
+++ src/main/java/jmcheynier/apps/portfolio/services/GithubService.java
@@ -0,0 +1,63 @@
+package jmcheynier.apps.portfolio.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+@Service
+public class GithubService {
+	
+	
+	@Autowired 
+	ResourceLoader resourceLoader;
+	
+	@Value("${api.github.url}")
+	private String apiGithubUrl;
+	
+	@Value("${api.github.secret}")
+	private String apiGithubSecret;
+	
+
+	private static final String QUERY = "queries/query.txt";
+	private static final MediaType JSONTYPE  = MediaType.get("application/json; charset=utf-8");
+	
+	
+	public String getGithubTags() throws IOException {
+		JSONObject json = new JSONObject();
+		 
+	    ClassLoader classLoader = getClass().getClassLoader();
+	    File file = new File(classLoader.getResource(QUERY).getFile());
+	    String querieString = FileUtils.readFileToString(file, "UTF-8");  
+	    json.put("query", querieString.trim());
+		
+	    System.out.println(json.toString());
+
+		OkHttpClient client = new OkHttpClient();
+		
+		RequestBody body = RequestBody.create(JSONTYPE, json.toString());
+		Request request = new Request.Builder()
+				.header("Authorization", apiGithubSecret)
+				.url(apiGithubUrl)
+				.post(body)
+				.build();
+		try (Response response = client.newCall(request).execute()) {
+			return response.body().string();
+		}
+		
+	}	
+
+}
