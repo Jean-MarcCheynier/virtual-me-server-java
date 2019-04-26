3bdde5e46b4a06491f0c5e406de2b03ad2193b82
TheMeatPacker <jean-marc.cheynier@fujitsu.com>
2019-04-26 15:37:42 +0200

    Organization and new Queries
    Add new controller and manage different call to github service in order to perform the different graphQL requests

---
+++ src/main/java/jmcheynier/apps/portfolio/controllers/GitHubController.java
@@ -0,0 +1,42 @@
+package jmcheynier.apps.portfolio.controllers;
+
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jmcheynier.apps.portfolio.services.GithubService;
+
+@RestController
+@RequestMapping("/api/v1/github")
+public class GitHubController {
+	
+	@Autowired
+	GithubService githubService;
+
+
+
+
+	@GetMapping("/service/{serviceName}")
+	public String commentsGithubGraphQL(@PathVariable("serviceName") String serviceName) throws IOException {
+		String res ="";
+		switch (serviceName) {
+		case "getRelease" :
+			res= githubService.getGithubReleases();
+			break;
+
+		default:
+			res = githubService.getGithubTags();
+			break;
+		}
+		return res;
+
+	}
+
+
+
+}
