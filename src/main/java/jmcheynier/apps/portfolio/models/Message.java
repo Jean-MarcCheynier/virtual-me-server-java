b81f50df4607a4a2c730fb4090a36af1039cb6ee
TheMeatPacker <jean-marc.cheynier@fujitsu.com>
2019-04-29 17:32:23 +0200

    Websocket configuration

---
+++ src/main/java/jmcheynier/apps/portfolio/models/Message.java
@@ -0,0 +1,27 @@
+package jmcheynier.apps.portfolio.models;
+
+public class Message {
+	
+	private String content;
+	
+	public Message() {
+		
+	}
+	
+	public Message(String content) {
+		this.content = content;
+	}
+	
+	
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
