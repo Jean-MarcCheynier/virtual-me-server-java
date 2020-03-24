5d2601c1f3389f47ab2ba9d3281d7b2940eac68f
Jean-Marc <jmcheynier@gmail.com>
2020-03-24 22:43:28 +0100

    test build prod

---
+++ src/main/java/jmcheynier/apps/portfolio/MyWebMvcConfigurer.java
@@ -0,0 +1,13 @@
+package jmcheynier.apps.portfolio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+  @Override
+  public void addViewControllers(ViewControllerRegistry reg) {
+    reg.addViewController("/ui/**").setViewName("forward:/index.html");
+  }
+}
