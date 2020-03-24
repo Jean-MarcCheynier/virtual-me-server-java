4d8466f4e93fc55bf19a411680e1f961fe38b5a1
Jean-Marc <jmcheynier@gmail.com>
2020-03-24 13:32:19 +0100

    [MIME FIX] test

---
+++ src/main/java/jmcheynier/apps/portfolio/MyMimeMapping.java
@@ -0,0 +1,18 @@
+package jmcheynier.apps.portfolio;
+
+import org.springframework.boot.web.server.MimeMappings;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyMimeMapping implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("js", "application/javascript; charset=utf-8");
+        factory.setMimeMappings(mappings);
+    }
+}
+
+
